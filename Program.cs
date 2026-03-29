f9842c1aa3df26fbd5e71b0e9b008163f1f8d87d
Jake Ferguson <jferguson15@my.wctc.edu>
2026-03-29 12:09:29 -0500

    [1.04] take 5

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+// deserialize mario json from file into List<DonkeyKong>
+List<Character> dks = JsonSerializer.Deserialize<List<Character>>(File.ReadAllText("dk.json"))!;
+// deserialize mario json from file into List<Mario>
+List<Character> marios = JsonSerializer.Deserialize<List<Character>>(File.ReadAllText("mario.json"))!;
+// combine all characters into 1 list
+List<Character> characters = [];
+characters.AddRange(dks);
+characters.AddRange(marios);
+
+// display all characters
+// foreach(Character character in characters)
+// {
+//   Console.WriteLine(character.Display());
+// }
+Console.Clear();
+
+// display first character
+// Console.WriteLine(characters.First().Display());
+
+// display the first 5 characters
+foreach(Character character in characters.Take(5))
+{
+  Console.WriteLine(character.Display());
+}
